1242a73fe2cb5430716e1e2620ce32846d896629
Szabó <martin77szabo@gmail.com>
2019-09-03 21:31:02 +0200

    Queue class added

---
+++ lmp.py
@@ -0,0 +1,64 @@
+import serial
+
+# Exceptions for Queue
+
+
+class QueueNonMatchingType(Exception):
+    pass
+
+
+class QueueIsEmpty(Exception):
+    pass
+
+
+# Classes
+
+
+class Queue(object):
+    # Basic queue
+
+    def __init__(self, type, initData=None):
+        self._type = type
+        self._data = []
+
+        if initData != None:
+            for element in initData:
+                self.insert(element)
+
+    def __str__(self):
+        return "<Queue of {} with size {}>".format(self.type, self.size)
+
+    def __repr__(self):
+        return "Queue(type={}, initData={})".format(self.type, self._data)
+
+    @property
+    def size(self):
+        return len(self._data)
+
+    @property
+    def type(self):
+        return self._type
+
+    def insert(self, element):
+
+        if type(element) != self.type:
+            raise QueueNonMatchingType("Expected {}, but got {}".format(self.type, type(element)))
+
+        self._data.append(element)
+
+    def peek(self):
+        if self.size == 0:
+            raise QueueIsEmpty()
+
+        return self._data[0]
+
+    def pop(self):
+        if self.size == 0:
+            raise QueueIsEmpty()
+
+        element = self._data.pop()
+
+        return element
+
+    def isEmpty(self):
+        return self.size == 0
